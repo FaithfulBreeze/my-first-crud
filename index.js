dc40453087f43157ebc694344170feb4c4ff53b8
FaithfulBreeze <faithfulbreeze@outlook.com>
2024-02-10 17:28:20 -0300

    Creating the pages and route

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const port = 3030
 http.createServer((req, res)=>{
-    res.end('<h1>Rodando!</h1>')
+    const url = req.url
+    if(url === '/'){
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.end(fs.readFileSync('./index.html'))
+    }else if(url.startsWith('/pages')){
+        const file = url.split('/')[2]
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.end(fs.readFileSync(path.join(__dirname, 'pages', file)))
+    }
 }).listen(port)
