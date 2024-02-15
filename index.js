8550221b450c0421a76faf05349fdb4fee67d96e
FaithfulBreeze <faithfulbreeze@outlook.com>
2024-02-15 18:12:41 -0300

    creating the CSS file, and the 404 page; Making the index.js more readable

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
-const port = 3030
-http.createServer((req, res)=>{
-    const url = req.url
-    if(url === '/'){
-        res.writeHead(200, {'Content-Type':'text/html'})
-        res.end(fs.readFileSync('./index.html'))
-    }else if(url.startsWith('/pages')){
-        const file = url.split('/')[2]
-        res.writeHead(200, {'Content-Type':'text/html'})
-        res.end(fs.readFileSync(path.join(__dirname, 'pages', file)))
-    }else if(url.startsWith('/src/images')){
-        const file = url.split('/')[3]
-        res.writeHead(200, {'Content-Type':'image/png'})
-        res.end(fs.readFileSync(path.join(__dirname, 'src', 'images', file)))
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const port = 3030;
+//Creates the Server!
+http.createServer((req, res) => {
+  //Just to get more readable
+  const url = req.url
+  //Sends the home page
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./index.html"))
+  } else {
+    //Get the name of the file
+    const file = url.split("/")[3]
+    //Using object bracket notation to exec the right method based on the url
+    const routes = {
+      html,
+      css,
+      png,
+      ico,
     }
+    //If the method from the routes obj gets an error (does not exist), then exec the 404 route
+    try {
+      routes[url.split(".")[1]]()
+    } catch (error) {
+      pageNotFound()
+    }
+    //The methods that are on the routes obj
+    function html() {
+      res.writeHead(200, { "Content-Type": "text/html" })
+      res.end(fs.readFileSync(path.join(__dirname, "src", "pages", file)))
+    }
+    function css() {
+      res.writeHead(200, { "Content-Type": "text/css" })
+      res.end(fs.readFileSync(path.join(__dirname, "src", "css", file)))
+    }
+    function png() {
+      res.writeHead(200, { "Content-Type": "image/png" })
+      res.end(fs.readFileSync(path.join(__dirname, "src", "images", file)))
+    }
+    function pageNotFound() {
+      res.writeHead(404, { "Content-Type": "text/html" })
+      res.end(fs.readFileSync("./404.html"))
+    }
+    function ico() {}
+  }
 }).listen(port)
