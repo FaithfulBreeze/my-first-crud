53dbaa5ed04e3ef5fd05f37b891bca7bce2ec30f
FaithfulBreeze <faithfulbreeze@outlook.com>
2024-02-15 10:16:11 -0300

    Creating folder and route for local icons

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ http.createServer((req, res)=>{
         const file = url.split('/')[2]
         res.writeHead(200, {'Content-Type':'text/html'})
         res.end(fs.readFileSync(path.join(__dirname, 'pages', file)))
+    }else if(url.startsWith('/src/images')){
+        const file = url.split('/')[3]
+        res.writeHead(200, {'Content-Type':'image/png'})
+        res.end(fs.readFileSync(path.join(__dirname, 'src', 'images', file)))
     }
 }).listen(port)
