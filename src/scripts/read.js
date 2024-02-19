5593e7124c530aef058471b86d16d66214ba69f1
FaithfulBreeze <faithfulbreeze@outlook.com>
2024-02-19 16:02:46 -0300

    search input to get info from a user

--- src/scripts/read.js
+++ src/scripts/read.js
@@ -1,11 +1,26 @@
-const ul = document.querySelector("#list")
+let users
+const list_ul = document.querySelector("#list")
+const res_ul = document.querySelector("#res")
+const buttonReadUser = document.querySelector('#button-read-user')
+buttonReadUser.addEventListener('click', (event)=>{
+    event.preventDefault()
+    const idInput = document.querySelector('#id-input')
+    const index = users.findIndex((usr)=>{
+        return usr.id === idInput.value
+    })
+    for(properties in users[index]){
+        const li = document.createElement('li')
+        li.innerHTML = `${properties}: ${users[index][properties]}`
+        res_ul.appendChild(li)
+    }
+})
 async function readUsers(){
     const data = await fetch('../../api/read')
-    const users = await data.json()
+    users = await data.json()
     for(user of users){
         const li = document.createElement('li')
         li.innerHTML = `Name: ${user.name}, id: ${user.id}`
-        ul.appendChild(li)
+        list_ul.appendChild(li)
     }
 }
 readUsers()
